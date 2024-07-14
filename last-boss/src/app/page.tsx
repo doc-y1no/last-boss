3a84e172a0645ac7cdcbc4041c82ce715019e115
Y.inoue <kisin1010@gmail.com>
2024-07-14 14:23:46 +0900

    追加

--- last-boss/src/app/page.tsx
+++ last-boss/src/app/page.tsx
@@ -3,7 +3,18 @@ import styles from './page.module.css';
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}></div>
+      <header>
+      <div>title</div>
+      </header>
+      <body>
+        <div>都道府県</div>
+        <tr>
+          <td>
+            チェックボックスの都道府県それぞれ表示
+          </td>
+        </tr>
+        <div>グラフ</div>
+      </body>
     </main>
   );
 }
