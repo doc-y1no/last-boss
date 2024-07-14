0b3ad29968ddb7afcbaacd6230f0c137ad1019ef
Y.inoue <kisin1010@gmail.com>
2024-07-14 17:23:36 +0900

    必要なコンポーネント作成　当てたい文字を入れるところまで作成

--- last-boss/src/app/page.tsx
+++ last-boss/src/app/page.tsx
@@ -1,21 +1,27 @@
 import styles from './page.module.css';
-import Header from './components/header';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Pre from './components/Prefectures';
+import Graph from './components/Graph';
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <header>
-          <Header title={'rasuboss'} />
+          <Header title={'超ラスボス課題'} />
       </header>
       <body>
-      <div>都道府県</div>
-          <tr>
-            <td>
-              チェックボックスの都道府県それぞれ表示
-            </td>
-          </tr>
-          <div>グラフ</div>
+      <div>
+          <Pre key={''} /> {/*APIキーをここで受け取るようにしてみる。 */}
+          <p>チェックボックスの都道府県それぞれ表示</p>
+      </div>
+          <div>
+            <Graph value={''} />
+          </div>
         </body>
+        <footer>
+          <Footer />
+        </footer>
     </main>
 
   );
