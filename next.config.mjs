18ad15829b18ec8b2be28437c22ff07848a734e4
Y.inoue <kisin1010@gmail.com>
2024-07-15 18:34:18 +0900

    vercelにAPIキーの設定ができていなかったため、APIキーの設定をして再度デプロイ完了

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_API_KEY: process.env.NEXT_PUBLIC_API_KEY,
+  },
+};
 
 export default nextConfig;
